49d88b20acbf63085778188ef675af57b9982652
Aaron <79734592+ChefBoyAC@users.noreply.github.com>
2025-02-22 04:31:50 -0500

    backend to frontend
    This is how to push the backend to the frontend

--- basketball-app/src/App.jsx
+++ basketball-app/src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect} from 'react'
 import './App.css'
 import Court from './court.jsx'
 
 function App() {
   const [position, setPosition] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/data")
+      .then((response) => response.json())
+      .then((data) => setData(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
 
   function handleCourtClick(event) {
     const rect = event.target.getBoundingClientRect();
@@ -20,6 +27,7 @@ function App() {
       {position && (
         <p>
           Shot Position: X = {Math.round(25 - Math.round(position.y)/10)}, Y = {Math.round(Math.round(position.x)/10)}
+          Percent: {data || "Loading..."}
         </p>
       )}
     </div>
